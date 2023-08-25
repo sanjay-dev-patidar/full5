a3d689226c2f8a1d2d36a64c0e5ca5b728cf2e0d
sanjay-dev-patidar <142602180+sanjay-dev-patidar@users.noreply.github.com>
2023-08-25 11:47:01 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: '/frontend/src/main.jsx', // Update this with your entry module's path
+    },
+  },
+});
