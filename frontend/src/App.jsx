e168c39800ca9779ff13599dc4d65a13024c6192
sanjay-dev-patidar <142602180+sanjay-dev-patidar@users.noreply.github.com>
2023-08-25 14:31:32 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-/* eslint-disable no-undef */
 
-import { useState } from "react";
-import { BrowserRouter, Route, Routes, useMatch } from "react-router-dom";
+
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navbar, Header, Home, Blogs, Footer } from "./components";
-
 import SectionWrapper from "./hoc/SectionWrapper";
 
 const HomeWithSectionWrapper = SectionWrapper(Home, "home");
@@ -19,22 +18,31 @@ const App = () => {
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
 
-          {/* Conditionally render the Header based on the route */}
           <Routes>
-            <Route path="/" element={<Header
-              activeButton={activeButton}
-              onSetActiveButton={setActiveButton}
-              setSelectedDocument={setSelectedDocument}
-              setSelectedCollectionAndTitle={setSelectedCollectionAndTitle}
-            />} />
-            <Route path="/blogs/*" element={BlogsHeader} />
+            <Route
+              path="/"
+              element={(
+                <Header
+                  activeButton={activeButton}
+                  onSetActiveButton={setActiveButton}
+                  setSelectedDocument={setSelectedDocument}
+                  setSelectedCollectionAndTitle={setSelectedCollectionAndTitle}
+                />
+              )}
+            />
+            <Route path="/blogs/*" element={<BlogsHeader />} />
           </Routes>
 
           <Routes>
-            <Route path="/" element={<HomeWithSectionWrapper
-              selectedDocument={selectedDocument}
-              selectedCollectionAndTitle={selectedCollectionAndTitle}
-            />} />
+            <Route
+              path="/"
+              element={(
+                <HomeWithSectionWrapper
+                  selectedDocument={selectedDocument}
+                  selectedCollectionAndTitle={selectedCollectionAndTitle}
+                />
+              )}
+            />
             <Route path="/blogs/*" element={<Blogs />} />
           </Routes>
         </div>
@@ -46,7 +54,6 @@ const App = () => {
     </BrowserRouter>
   );
 };
-
 const BlogsHeader = () => {
   const match = useMatch("/blogs/*"); // Match any route under /blogs
   if (!match) return null; // Don't render the header on other routes
