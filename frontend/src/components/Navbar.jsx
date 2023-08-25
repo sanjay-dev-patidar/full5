d4893ef1715db68524d1afc61063929c49648cd5
sanjay-dev-patidar <142602180+sanjay-dev-patidar@users.noreply.github.com>
2023-08-25 14:33:44 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
-import { NavLink, useLocation } from "react-router-dom"; // Import NavLink from react-router-dom
+import { NavLink, useLocation } from "react-router-dom";
 import Typed from "react-typed";
-
 import { logo } from "../assets";
-
 import "./navbar.css";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
-  const location = useLocation(); // Get the current location
-
-  const isBlogsPage = location.pathname.startsWith("/blogs"); // Check if the current page is blogs
+  const location = useLocation();
+  const isBlogsPage = location.pathname.startsWith("/blogs");
   const typedTexts = [
     "Spark Minds",
     "Explore Ideas",
@@ -39,18 +36,15 @@ const Navbar = () => {
             <Typed strings={typedTexts} typeSpeed={80} backSpeed={40} loop />
           </p>
         </NavLink>
-        {!isBlogsPage && ( // Display Blogs NavLink only if not on Blogs page
+        {!isBlogsPage && (
           <NavLink
             to="/blogs"
             activeClassName="active"
-            target="_blank"
-            rel="noopener noreferrer"
             className="nav-link"
           >
             Blogs
           </NavLink>
         )}
-       
       </div>
     </nav>
   );
